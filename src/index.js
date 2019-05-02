c85a39ce6a334fb00f2c2e605e1eda6f15d895ea
Lorena Cunha <lorenapassagem@gmail.com>
2019-05-02 20:24:51 +0000

    enviado arquivos atualizados

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon =
- ` <img src='img/icon-collabcode.png' 
+const $icon = ` <img src='img/icon-collabcode.png' 
  alt='Gueio mascote collab'/> `;
- const $iconC = document.createElement("article");
+ 
+const $iconC = document.createElement("article");
+ const $cardC = `<img class="card-c" src="img/icon-c.png" 
+                alt="imagem de um livro de C++" class="iconC"/>`
 
 $memoryCard.classList.add("memory-card");
 $iconC.classList.add("iconC");
 
 $root.insertBefore($memoryCard, null);
-$root.insertBefore($iconC, $memoryCard);
+$root.insertBefore($iconC, null);
 
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$iconC.insertAdjacentHTML("afterbegin", $cardC);
 
 
 
