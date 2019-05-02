cff0f0dc539038f3a6ceaf484ccda1176fbfcb9c
Lorena Cunha <lorenapassagem@gmail.com>
2019-05-02 22:20:48 +0000

    refazendo o desafio

--- src/icon-c.js
+++
@@ -1,9 +0,0 @@
-// pegar um id root para inserir um article na tela
-const $root = document.querySelector("#root");
-// cria uma tag article e coloca ela dentro do de uma const
-const $iconC = document.createElement("article");
-
-// insere uma class no article criado acima
-$iconC.classList.add("iconC");
-$root.insertBefore($iconC, memoryCard);
- 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
+// Constates e variaveis
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon = ` <img src='img/icon-collabcode.png' 
+const $memoryCardFront = document.createElement("article");
+
+const $iconCollab = 
+` <img src='img/icon-collabcode.png' 
  alt='Gueio mascote collab'/> `;
- 
-const $iconC = document.createElement("article");
- const $cardC = `<img class="card-c" src="img/icon-c.png" 
-                alt="imagem de um livro de C++" class="iconC"/>`
 
+const $iconC = 
+`<img src="img/icon-c.png" 
+alt="imagem de um livro de C++" class="icon"/>`
+
+//  add as classe do css
 $memoryCard.classList.add("memory-card");
-$iconC.classList.add("iconC");
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
 
+// insere os elementos na tela
 $root.insertBefore($memoryCard, null);
-$root.insertBefore($iconC, null);
+$root.insertBefore($memoryCardFront, $memoryCard);
 
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
-
-$iconC.insertAdjacentHTML("afterbegin", $cardC);
+//  insere as tags de img
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
 
 
 
